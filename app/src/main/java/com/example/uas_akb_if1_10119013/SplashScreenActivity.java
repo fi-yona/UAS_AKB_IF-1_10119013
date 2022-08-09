7608abf53e5633fd580e7ad42c75b16aa5ba140f
fi-yona <fionaavila15@gmail.com>
2022-08-09 16:38:55 +0700

    Add Layout Input For Login

--- app/src/main/java/com/example/uas_akb_if1_10119013/SplashScreenActivity.java
+++ app/src/main/java/com/example/uas_akb_if1_10119013/SplashScreenActivity.java
@@ -2,13 +2,28 @@ package com.example.uas_akb_if1_10119013;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private Button button_mulai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        button_mulai = findViewById(R.id.rectangle_button_mulai);
+
+        button_mulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
