537c201fc3de93e1a85349dcb859066846d92092
fi-yona <fionaavila15@gmail.com>
2022-08-09 18:22:56 +0700

    Edit Navigation Menu Profil

---
+++ app/src/main/java/com/example/uas_akb_if1_10119013/ui/profil/ProfilFragment.java
@@ -0,0 +1,30 @@
+package com.example.uas_akb_if1_10119013.ui.profil;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.uas_akb_if1_10119013.R;
+import com.example.uas_akb_if1_10119013.databinding.FragmentProfilBinding;
+
+public class ProfilFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
