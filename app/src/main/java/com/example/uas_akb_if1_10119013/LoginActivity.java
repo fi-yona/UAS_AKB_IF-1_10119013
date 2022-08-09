cb6279d846e42794443fb4f11192edd366ad2852
fi-yona <fionaavila15@gmail.com>
2022-08-09 17:46:27 +0700

    Add Template Layout for Navigation Drawer Menu

--- app/src/main/java/com/example/uas_akb_if1_10119013/LoginActivity.java
+++ app/src/main/java/com/example/uas_akb_if1_10119013/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.uas_akb_if1_10119013;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button button_enter_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button_enter_login = findViewById(R.id.rectangle_button_login);
+
+        button_enter_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
