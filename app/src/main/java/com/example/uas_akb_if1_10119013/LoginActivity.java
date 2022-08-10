31787a96e5f0d3e221276a082fe77579f65080ae
fi-yona <fionaavila15@gmail.com>
2022-08-11 00:49:09 +0700

    Add Function for Firebase Authentication Login and Register

--- app/src/main/java/com/example/uas_akb_if1_10119013/LoginActivity.java
+++ app/src/main/java/com/example/uas_akb_if1_10119013/LoginActivity.java
@@ -1,38 +1,83 @@
 package com.example.uas_akb_if1_10119013;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.content.pm.ActivityInfo;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button button_enter_login;
     private Button button_register;
+    private EditText input_email, input_password;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null)  ab.setTitle("Login");
 
-        button_enter_login = findViewById(R.id.rectangle_button_login);
+        input_email = (EditText) findViewById(R.id.input_username);
+        input_password = (EditText) findViewById(R.id.input_password);
+        button_enter_login = (Button) findViewById(R.id.rectangle_button_login);
+        button_register = (Button) findViewById(R.id.rectangle_button_daftar);
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+            finish();
+        }
+
         button_enter_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, MenuActivity.class);
-                startActivity(i);
+
+                String email = input_email.getText().toString();
+                String password = input_password.getText().toString();
+                if (email.isEmpty()) input_email.setError("Email tidak boleh kosong");
+                else if (password.isEmpty()) input_password.setError("Password tidak boleh kosong");
+                else {
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, "Login berhasil", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Login gagal", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
             }
         });
 
-        button_register = findViewById(R.id.rectangle_button_daftar);
         button_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(i);
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }
+
+
 }
--- app/src/main/java/com/example/uas_akb_if1_10119013/RegisterActivity.java
+++ app/src/main/java/com/example/uas_akb_if1_10119013/RegisterActivity.java
@@ -1,13 +1,76 @@
 package com.example.uas_akb_if1_10119013;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
+    private EditText input_email, input_password;
+    private Button button_back_login, button_register;
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null)  ab.setTitle("Daftar Akun");
+
+        auth = FirebaseAuth.getInstance();
+
+        input_email = (EditText) findViewById(R.id.regEmail);
+        input_password = (EditText) findViewById(R.id.regPassword);
+        button_back_login = (Button) findViewById(R.id.back_login);
+        button_register = (Button) findViewById(R.id.regUser);
+
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = input_email.getText().toString().trim();
+                String password = input_password.getText().toString().trim();
+                if (email.isEmpty()) input_email.setError("Email tidak boleh kosong");
+                else if (password.isEmpty()) input_password.setError("Password tidak boleh kosong");
+                else if (password.length() < 6) input_password.setError("Password harus lebih dari 6 karakter");
+                else {
+                    auth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "Akun berhasil dibuat", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                }
+            }
+        });
+
+        button_back_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
     }
 }
