3f578fe167e4c759c76bfe4044706c65b13d6326
fi-yona <fionaavila15@gmail.com>
2022-08-11 16:58:30 +0700

    Function Reset Password with Email

--- app/src/main/java/com/example/uas_akb_if1_10119013/ResetPasswordActivity.java
+++ app/src/main/java/com/example/uas_akb_if1_10119013/ResetPasswordActivity.java
@@ -1,15 +1,27 @@
 package com.example.uas_akb_if1_10119013;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
     private Button button_back_login;
+    private EditText input_email_reset;
+    private Button button_kirim_link;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +29,31 @@ public class ResetPasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reset_password);
 
         button_back_login = findViewById(R.id.back_login_reset);
+        input_email_reset = findViewById(R.id.regEmail_reset);
+        button_kirim_link = findViewById(R.id.reset_pass);
 
         button_back_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+            }
+        });
+
+        auth = FirebaseAuth.getInstance();
+
+        button_kirim_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.sendPasswordResetEmail(input_email_reset.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ResetPasswordActivity.this,"Link telah dikirimkan ke email Anda!", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(ResetPasswordActivity.this,task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
     }
